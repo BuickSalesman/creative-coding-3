e4662bcd62db1eec67b4f014f19fc914afbbfbf3
BuickSalesman <adam.odonnell.e@gmail.com>
2025-01-29 19:41:36 -0500

    added square switching on click and default state on page load

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 const rightSquare = document.getElementById("right-square");
+const rightHole = document.getElementById("right-hole");
 const leftSquare = document.getElementById("left-square");
+const leftHole = document.getElementById("left-hole");
 let isSquareRight = false;
 
-rightSquare.addEventListener("click", () => {
+checkIsSquareRight();
+
+rightHole.addEventListener("click", () => {
   console.log("hello");
   isSquareRight = true;
   checkIsSquareRight();
 });
 
-leftSquare.addEventListener("click", () => {
+leftHole.addEventListener("click", () => {
   isSquareRight = false;
   checkIsSquareRight();
 });
@@ -16,7 +20,11 @@ leftSquare.addEventListener("click", () => {
 function checkIsSquareRight() {
   if (isSquareRight === true) {
     console.log("true");
+    rightSquare.style.backgroundColor = "white";
+    leftSquare.style.backgroundColor = "brown";
   } else {
     console.log("false");
+    rightSquare.style.backgroundColor = "green";
+    leftSquare.style.backgroundColor = "white";
   }
 }
