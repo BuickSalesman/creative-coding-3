2934411914806c2c3b67870a007aecee5f2b4df7
BuickSalesman <adam.odonnell.e@gmail.com>
2025-01-29 20:35:38 -0500

    restructured code to be a bit more readble

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-const rightSquare = document.getElementById("right-square");
-const rightHole = document.getElementById("right-hole");
 const leftSquare = document.getElementById("left-square");
 const leftHole = document.getElementById("left-hole");
+
+const rightSquare = document.getElementById("right-square");
+const rightHole = document.getElementById("right-hole");
+
 let isSquareRight = false;
 let isRightSquareGrabbed = false;
 let isLeftSquareGrabbed = false;
@@ -18,15 +20,6 @@ document.addEventListener("mouseup", () => {
   isMouseDown = false;
 });
 
-rightSquare.addEventListener("mousedown", () => {
-  isLeftSquareGrabbed = false;
-  isRightSquareGrabbed = true;
-});
-
-rightSquare.addEventListener("mouseup", () => {
-  isRightSquareGrabbed = false;
-});
-
 leftSquare.addEventListener("mousedown", () => {
   isRightSquareGrabbed = false;
   isLeftSquareGrabbed = true;
@@ -36,15 +29,44 @@ leftSquare.addEventListener("mouseup", () => {
   isLeftSquareGrabbed = false;
 });
 
+rightSquare.addEventListener("mousedown", () => {
+  isLeftSquareGrabbed = false;
+  isRightSquareGrabbed = true;
+});
+
+rightSquare.addEventListener("mouseup", () => {
+  isRightSquareGrabbed = false;
+});
+
+leftHole.addEventListener("mouseenter", () => {
+  if (!isMouseDown) return;
+
+  if (isRightSquareGrabbed === true) {
+    timesPassed++;
+    isRightSquareGrabbed = false;
+    isLeftSquareGrabbed = true;
+    rightSquare.textContent = null;
+    rightSquare.style.backgroundColor = "green";
+    leftSquare.style.backgroundColor = "white";
+    leftSquare.textContent = `times passed: ${timesPassed}`;
+  } else if (isMouseDown === true && isRightSquareGrabbed === false) {
+    timesPassed++;
+    rightSquare.style.backgroundColor = "white";
+    leftSquare.style.backgroundColor = "brown";
+    rightSquare.textContent = `times passed: ${timesPassed}`;
+  }
+});
+
 rightHole.addEventListener("mouseenter", () => {
+  if (!isMouseDown) return;
   if (isLeftSquareGrabbed === true) {
     timesPassed++;
     isLeftSquareGrabbed = false;
     isRightSquareGrabbed = true;
-    rightSquare.style.backgroundColor = "white";
-    rightSquare.textContent = `times passed: ${timesPassed}`;
-    leftSquare.style.backgroundColor = "brown";
     leftSquare.textContent = null;
+    rightSquare.style.backgroundColor = "white";
+    leftSquare.style.backgroundColor = "brown";
+    rightSquare.textContent = `times passed: ${timesPassed}`;
   } else if (isMouseDown === true && isLeftSquareGrabbed === false) {
     timesPassed++;
 
@@ -54,32 +76,11 @@ rightHole.addEventListener("mouseenter", () => {
   }
 });
 
-leftHole.addEventListener("mouseenter", () => {
-  if (isRightSquareGrabbed === true) {
-    timesPassed++;
-
-    isRightSquareGrabbed = false;
-    isLeftSquareGrabbed = true;
-    rightSquare.style.backgroundColor = "green";
-    rightSquare.textContent = null;
-    leftSquare.style.backgroundColor = "white";
-    leftSquare.textContent = `times passed: ${timesPassed}`;
-  } else if (isMouseDown === true && isRightSquareGrabbed === false) {
-    rightSquare.style.backgroundColor = "white";
-    timesPassed++;
-
-    rightSquare.textContent = `times passed: ${timesPassed}`;
-    leftSquare.style.backgroundColor = "brown";
-  }
-});
-
 function checkIsSquareRight() {
   if (isSquareRight === true) {
-    console.log("true");
     rightSquare.style.backgroundColor = "white";
     leftSquare.style.backgroundColor = "brown";
   } else {
-    console.log("false");
     rightSquare.style.backgroundColor = "green";
     leftSquare.style.backgroundColor = "white";
   }
