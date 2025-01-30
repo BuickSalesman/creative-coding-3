d858a285e10bba3c99ba0567f0fa523d2395c96c
BuickSalesman <adam.odonnell.e@gmail.com>
2025-01-29 20:16:55 -0500

    follows mouse on drag into each container

--- script.js
+++ script.js
@@ -5,17 +5,16 @@ const leftHole = document.getElementById("left-hole");
 let isSquareRight = false;
 let isRightSquareGrabbed = false;
 let isLeftSquareGrabbed = false;
+let isMouseDown = false;
 
 checkIsSquareRight();
 
-rightHole.addEventListener("click", () => {
-  isSquareRight = true;
-  checkIsSquareRight();
+document.addEventListener("mousedown", () => {
+  isMouseDown = true;
 });
 
-leftHole.addEventListener("click", () => {
-  isSquareRight = false;
-  checkIsSquareRight();
+document.addEventListener("mouseup", () => {
+  isMouseDown = false;
 });
 
 rightSquare.addEventListener("mousedown", () => {
@@ -38,6 +37,30 @@ leftSquare.addEventListener("mousedown", () => {
 leftSquare.addEventListener("mouseup", () => {
   isLeftSquareGrabbed = false;
   console.log(`still hoding left square: ${isLeftSquareGrabbed}`);
+});
+
+rightHole.addEventListener("mouseenter", () => {
+  if (isLeftSquareGrabbed === true) {
+    isLeftSquareGrabbed = false;
+    isRightSquareGrabbed = true;
+    rightSquare.style.backgroundColor = "white";
+    leftSquare.style.backgroundColor = "brown";
+  } else if (isMouseDown === true && isLeftSquareGrabbed === false) {
+    rightSquare.style.backgroundColor = "green";
+    leftSquare.style.backgroundColor = "white";
+  }
+});
+
+leftHole.addEventListener("mouseenter", () => {
+  if (isRightSquareGrabbed === true) {
+    isRightSquareGrabbed = false;
+    isLeftSquareGrabbed = true;
+    rightSquare.style.backgroundColor = "green";
+    leftSquare.style.backgroundColor = "white";
+  } else if (isMouseDown === true && isRightSquareGrabbed === false) {
+    rightSquare.style.backgroundColor = "white";
+    leftSquare.style.backgroundColor = "brown";
+  }
 });
 
 function checkIsSquareRight() {
