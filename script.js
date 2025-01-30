9dd610b1933e72f93c378a63f7a1a2153b4f66fa
BuickSalesman <adam.odonnell.e@gmail.com>
2025-01-29 20:30:38 -0500

    added times passed feature

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let isSquareRight = false;
 let isRightSquareGrabbed = false;
 let isLeftSquareGrabbed = false;
 let isMouseDown = false;
+let timesPassed = 0;
 
 checkIsSquareRight();
 
@@ -20,45 +21,54 @@ document.addEventListener("mouseup", () => {
 rightSquare.addEventListener("mousedown", () => {
   isLeftSquareGrabbed = false;
   isRightSquareGrabbed = true;
-  console.log(`right square grabbed: ${isRightSquareGrabbed}`);
 });
 
 rightSquare.addEventListener("mouseup", () => {
   isRightSquareGrabbed = false;
-  console.log(`still hoding right square: ${isRightSquareGrabbed}`);
 });
 
 leftSquare.addEventListener("mousedown", () => {
   isRightSquareGrabbed = false;
   isLeftSquareGrabbed = true;
-  console.log(`left square grabbed: ${isLeftSquareGrabbed}`);
 });
 
 leftSquare.addEventListener("mouseup", () => {
   isLeftSquareGrabbed = false;
-  console.log(`still hoding left square: ${isLeftSquareGrabbed}`);
 });
 
 rightHole.addEventListener("mouseenter", () => {
   if (isLeftSquareGrabbed === true) {
+    timesPassed++;
     isLeftSquareGrabbed = false;
     isRightSquareGrabbed = true;
     rightSquare.style.backgroundColor = "white";
+    rightSquare.textContent = `times passed: ${timesPassed}`;
     leftSquare.style.backgroundColor = "brown";
+    leftSquare.textContent = null;
   } else if (isMouseDown === true && isLeftSquareGrabbed === false) {
+    timesPassed++;
+
     rightSquare.style.backgroundColor = "green";
     leftSquare.style.backgroundColor = "white";
+    leftSquare.textContent = `times passed: ${timesPassed}`;
   }
 });
 
 leftHole.addEventListener("mouseenter", () => {
   if (isRightSquareGrabbed === true) {
+    timesPassed++;
+
     isRightSquareGrabbed = false;
     isLeftSquareGrabbed = true;
     rightSquare.style.backgroundColor = "green";
+    rightSquare.textContent = null;
     leftSquare.style.backgroundColor = "white";
+    leftSquare.textContent = `times passed: ${timesPassed}`;
   } else if (isMouseDown === true && isRightSquareGrabbed === false) {
     rightSquare.style.backgroundColor = "white";
+    timesPassed++;
+
+    rightSquare.textContent = `times passed: ${timesPassed}`;
     leftSquare.style.backgroundColor = "brown";
   }
 });
