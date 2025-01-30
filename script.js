0a98ccc9dabb6504317eba870f6435098dc4f13a
BuickSalesman <adam.odonnell.e@gmail.com>
2025-01-29 19:52:32 -0500

    added event listeners to more squares

--- script.js
+++ script.js
@@ -3,11 +3,12 @@ const rightHole = document.getElementById("right-hole");
 const leftSquare = document.getElementById("left-square");
 const leftHole = document.getElementById("left-hole");
 let isSquareRight = false;
+let isRightSquareGrabbed = false;
+let isLeftSquareGrabbed = false;
 
 checkIsSquareRight();
 
 rightHole.addEventListener("click", () => {
-  console.log("hello");
   isSquareRight = true;
   checkIsSquareRight();
 });
@@ -15,6 +16,28 @@ rightHole.addEventListener("click", () => {
 leftHole.addEventListener("click", () => {
   isSquareRight = false;
   checkIsSquareRight();
+});
+
+rightSquare.addEventListener("mousedown", () => {
+  isLeftSquareGrabbed = false;
+  isRightSquareGrabbed = true;
+  console.log(`right square grabbed: ${isRightSquareGrabbed}`);
+});
+
+rightSquare.addEventListener("mouseup", () => {
+  isRightSquareGrabbed = false;
+  console.log(`still hoding right square: ${isRightSquareGrabbed}`);
+});
+
+leftSquare.addEventListener("mousedown", () => {
+  isRightSquareGrabbed = false;
+  isLeftSquareGrabbed = true;
+  console.log(`left square grabbed: ${isLeftSquareGrabbed}`);
+});
+
+leftSquare.addEventListener("mouseup", () => {
+  isLeftSquareGrabbed = false;
+  console.log(`still hoding left square: ${isLeftSquareGrabbed}`);
 });
 
 function checkIsSquareRight() {
